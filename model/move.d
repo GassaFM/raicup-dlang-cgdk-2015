023bf640ee5fde997fa4976f523e056c338b84db
Ivan Kazmenko <gassafm@gmail.com>
2015-11-24 17:06:47 +0300

    default values for move

--- model/move.d
+++ model/move.d
@@ -2,10 +2,10 @@ module model.move;
 
 class Move
 {
-    double enginePower;
-    bool isBrake;
-    double wheelTurn;
-    bool isThrowProjectile;
-    bool isUseNitro;
-    bool isSpillOil;
+    double enginePower = 0.0;
+    bool isBrake = false;
+    double wheelTurn = 0.0;
+    bool isThrowProjectile = false;
+    bool isUseNitro = false;
+    bool isSpillOil = false;
 }
